d1d8d84cfecbd3aded6eeca6c5653668180c4197
KohnoA <san.kohno@yandex.ru>
2022-07-29 03:11:17 +0300

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -38,7 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let separateArr = []; 
+    let removeZeros = [];
+    let result = [];
+
+    for(let i = 0; i < expr.length; i += 10) {
+        separateArr.push(expr.slice(i, 10 + i));
+    }
+
+    separateArr.map(item => {
+        removeZeros.push(item.replace(/^0+/, '')
+        .replace(/10/g, '.')
+        .replace(/11/g, '-'));
+    });
+
+    for(let j = 0; j < separateArr.length; j++) {
+        for(let key in MORSE_TABLE) {
+            if(removeZeros[j] === key) {
+                result.push(MORSE_TABLE[key]);
+            } 
+        }
+        if(removeZeros[j] === '**********') {
+            result.push(' ');
+        }
+    }
+    
+    return result.join('');
 }
 
 module.exports = {
